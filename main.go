65183889d1abcbdb6165ca94469b990938751af8
Sebin CM <sebinsebzz2002@gmail.com>
2023-11-23 21:48:23 +0530

    array: delete

--- main.go
+++ main.go
@@ -19,16 +19,21 @@ func (re *Array) insert(pos int, num int) {
 	re.arr[pos] = num
 }
 
+func (re *Array) delete(pos int){
+	if pos < 1 || pos > len(re.arr) {
+		fmt.Println("Position out of range")
+		return
+	}
+	re.arr= append(re.arr[:pos] , re.arr[pos+1:]...)
+}
 
 func main() {
 	a := Array{
 		max: 5,
-		arr: []int{10,21,23,89},
+		arr: []int{10,21,23,89,87},
 	}
-	// a.insert(0, 10)
-	// a.insert(1,21)
-	// a.insert(2,23)
-	// a.insert(1,89)
+	a.insert(1,89)
+	a.delete(2)
 
 	fmt.Println(a.arr)
 }
